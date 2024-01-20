302ce6f0070d3d17cd5f1250cf8e1faf7660665c
ikedoji45anne <148642109+ikedoji45anne@users.noreply.github.com>
2024-01-20 01:43:25 +0100

    Added the functionalities with the Api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+function refreshweather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city");
+    cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+    
+}
+
+
+function searchCity(city) {
+    let apiKey = "65fb7046d82c7c4d3377a8b9tfd374o0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`
+axios.get(apiUrl).then(refreshweather);
+}
+
+
+
+
 function handleSearch(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-
-    let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = searchInput.value;
+ 
+    searchCity(searchInput.value);
 }
 
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+searchCity("Paris");
